2f6074e2d2d90cf2c14b590279db49825e008c53
Olawale Adedamola <olawaleade15@gmail.com>
2022-09-16 13:20:30 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * print_numbers - prints number from 0-9
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 9)
+	{
+		_putchar("%d", i);
+		i++
+	}
+}
