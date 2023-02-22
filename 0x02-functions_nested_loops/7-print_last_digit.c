4b4893274041e7f9d0de28cc84905efe60249cd8
Olawale Adedamola <olawaleade15@gmail.com>
2023-02-21 19:58:50 -0800

    resolved errors

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,13 +11,13 @@ int print_last_digit(int n)
 {
 	if (n >= 0 && n <= 9)
 	{
-		_putchar('n' + 'n');
-		return (n);
+		_putchar((n % 10) + '0');
+		return (n % 10);
 	}
 	else
 	{
 		n = n * -1;
-		 _putchar((n % 10) + 'n');
+		 _putchar((n % 10) + '0');
 		return (n % 10);
 	}
 }
