422dc336d01d19297830f33a72f0ff7a5105aa4b
Olawale Adedamola <olawaleade15@gmail.com>
2023-02-22 08:05:31 -0800

    resolved errors

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,17 +8,16 @@
 
 void jack_bauer(void)
 {
-int h1;
-int h2;
-int m1;
-int m2;
+int h1, h2, m1, m2;
 
 h1 = '0';
 while (h1 <= '2')
 {
 	h2 = '0';
-	while (h2 <= '3')
+	while (h2 <= '9')
 	{
+		if (h1 == '2' && h2 == '4')
+			break;
 		m1 = '0';
 		while (m1 <= '5')
 		{
