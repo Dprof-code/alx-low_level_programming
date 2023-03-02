dbc792ad41028ffbb75533e94cfd41b9c265e0cc
Olawale Adedamola <olawaleade15@gmail.com>
2023-03-01 19:11:31 -0800

    7. Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - prints half of a string
+ * @str: character
+ *
+ * Returns: nothing
+ */
+void puts_half(char *str)
+{
+	int i, len, start;
+	/*get length of string*/
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	/**
+	 * determine starting point of the part of the string
+	 * to be printed based on if string is odd or even
+	 */
+
+	start = 0;
+
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = (len - 1) / 2;
+
+	for (i = start; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
