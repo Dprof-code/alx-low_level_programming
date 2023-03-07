45d163750d4f7c4e2f196497ab85b58f8d74d766
Olawale Adedamola <olawaleade15@gmail.com>
2023-03-06 18:00:47 -0800

    random task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: bytes from src
+ *
+ * Return: pointer to resulting string
+ */
+char *_strncat(char *dest, char *src, int n);
