c06339aa95ed773cfd04e116b3ad945e95562d07
Olawale Adedamola <olawaleade15@gmail.com>
2023-03-07 15:52:52 -0800

    resolved checker errors

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,13 +15,16 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (j = 0; needle[j] != '\0'; j++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (i = 0; haystack[i] != '\0'; i++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[j] == haystack[i])
-				return (haystack + i);
+			if (needle[j] != haystack[i + j])
+				break;
 		}
+
+		if (!needle[j]) /* found needle */
+			return (haystack + i);
 	}
 
 	return (NULL);
