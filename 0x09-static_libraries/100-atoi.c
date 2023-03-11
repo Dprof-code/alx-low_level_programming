6c6109284097fd40b016972fd0ccefd5e9c13717
Olawale Adedamola <olawaleade15@gmail.com>
2023-03-10 19:29:07 -0800

    resolved checker errors

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -10,5 +10,23 @@
 
 int _atoi(char *s)
 {
-	return (0);
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			result = result * 10 + (s[i] - '0');
+		else
+			break;
+	}
+
+	return (sign * result);
 }
--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -11,7 +11,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	printf("Two strings");
+	int i = 0;
 
-	return (0);
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+
+		i++;
+	}
+
+	return (s1[i] - s2[i]);
 }
