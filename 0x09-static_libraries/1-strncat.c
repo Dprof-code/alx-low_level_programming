a78e099cce6b8a42604c2c2d350b0c73aa296c1d
Olawale Adedamola <olawaleade15@gmail.com>
2023-03-12 16:27:22 -0700

    resolved checker errors

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -9,4 +9,22 @@
  *
  * Return: pointer to resulting string
  */
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+
+	while (dest[dest_len] != '\0')
+		dest_len++;
+
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+
+	dest[dest_len + i] = '\0';
+	return (dest);
+
+}
