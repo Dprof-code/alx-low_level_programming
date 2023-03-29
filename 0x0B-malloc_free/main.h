cb32e5ed8963da653b899cf4546ec9fb3e5d6987
Olawale Adedamola <olawaleade15@gmail.com>
2023-03-28 18:26:42 -0700

    made corrections

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
+#include <string.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
