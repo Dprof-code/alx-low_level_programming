f26466c143785a6b8a357543a4e3810716eb1a21
Olawale Adedamola <olawaleade15@gmail.com>
2023-04-03 21:14:28 -0700

    resolved checker errors

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,5 +9,14 @@
 
 void free_list(list_t *head)
 {
-	free(head);
+	list_t *temp_node;
+	/* temp_node stores current node that is freed */
+
+	while (head != NULL)
+	{
+		temp_node = head;
+		head = head->next;
+		free(temp_node->str);
+		free(temp_node);
+	}
 }
