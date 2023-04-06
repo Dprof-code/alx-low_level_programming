1f3ccc07c3559d8bd6c9dc9191d228f244f6024c
Olawale Adedamola <olawaleade15@gmail.com>
2023-04-05 20:21:45 -0700

    resolved checker errors

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,6 +7,7 @@
  * @n: list data
  * Return: address of the new node
  * else, return NULL
+ * if idx is impossible, do not add and return NULL
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -14,16 +15,29 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *node = *head;
 	listint_t *new_node = malloc(sizeof(listint_t));
 
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->n = n;
 	new_node->next = NULL;
 
-	if (idx > print_listint(*head))
-		return (NULL);
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-	while (idx != 1)
+	while (idx > 1 && node != NULL)
 	{
 		node = node->next;
 		idx--;
+	}
+
+	if (node == NULL)
+	{
+		free(new_node);
+		return (NULL);
 	}
 
 	new_node->next = node->next;
