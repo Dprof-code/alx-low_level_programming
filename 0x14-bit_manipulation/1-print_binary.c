1e7a1987357dc94d841d3fcfa4fdf05fb0230b76
Olawale Adedamola <olawaleade15@gmail.com>
2023-04-11 06:18:16 -0700

    resolved checker errors

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,21 +8,8 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1UL << (sizeof(n) * 8 - 1);
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	while (mask != 0)
-	{
-		if ((n & mask) != 0)
-			_putchar('1');
-		else
-			_putchar('0');
-
-		mask >>= 1;
-	}
+	_putchar((n & 1) + '0');
 }
