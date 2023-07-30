7697c6943eb069920c57a699050a64e6ed4252b2
Dprof-Code <olawaleade15@gmail.com>
2023-07-30 00:34:50 -0500

    resolved errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,66 +1,71 @@
 #include "main.h"
 
 /**
- * error_exit - error handler
- *
- * @message: error message
- * @exit_code: error code
- * Return: nothing
+ * error_handle - handles error
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-
-void error_exit(const char *message, int exit_code)
+void error_handle(int file_from, int file_to, char *argv[])
 {
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(exit_code);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 }
 
 /**
  * main - copies the content of a file to another file.
- *
  * @argc: arguments count
  * @argv: array of passed arguments
- * Return: 0;
+ * Return: Always 0.
  */
-
 int main(int argc, char *argv[])
 {
-	const char *file_from = argv[1];
-	const char *file_to = argv[2];
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written;
-	int fd_from;
-	int fd_to;
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
 
 	if (argc != 3)
-		error_exit("Usage: cp file_from file_to", 97);
-
-	file_from = argv[1];
-	file_to = argv[2];
-	fd_from = open(file_from, O_RDONLY);
-
-	if (fd_from == -1)
-		error_exit("Error: Can't read from file", 98);
-
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-
-	if (fd_to == -1)
-		error_exit("Error: Can't write to file", 99);
-
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
-			error_exit("Error: Can't write to file", 99);
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
 	}
 
-	if (bytes_read == -1)
-		error_exit("Error: Can't read from file", 98);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	void error_handle(int file_from, int file_to, char *argv[])(file_from, file_to, argv);
 
-	if (close(fd_from) == -1)
-		error_exit("Error: Can't close fd", 100);
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			void error_handle(int file_from, int file_to, char *argv[])(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			void error_handle(int file_from, int file_to, char *argv[])(0, -1, argv);
+	}
 
-	if (close(fd_to) == -1)
-		error_exit("Error: Can't close fd", 100);
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,8 +12,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void error_exit(const char *message, int exit_code);
-void print_error(const char *message);
+void error_handle(int file_from, int file_to, char *argv[]) void print_error(const char *message);
 void print_elf_header_info(Elf64_Ehdr elf_header);
 
 #endif
