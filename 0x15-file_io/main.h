f200c7e3ecda75167f53e694a5ee02c88b5a05b9
Dprof-Code <olawaleade15@gmail.com>
2023-07-30 00:36:20 -0500

    fixed

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,7 +12,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void error_handle(int file_from, int file_to, char *argv[]) void print_error(const char *message);
+void error_handle(int file_from, int file_to, char *argv[]);
 void print_elf_header_info(Elf64_Ehdr elf_header);
 
 #endif
