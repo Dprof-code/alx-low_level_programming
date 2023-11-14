11c5a11d1c7909a2eb0d38abfb383611b7f40b0a
dprof-code <olawaleade15@gmail.com>
2023-11-13 20:15:23 -0800

    finished an advanced task

---
+++ 0x18-dynamic_libraries/func.c
@@ -0,0 +1,61 @@
+#include "func.h"
+
+/**
+ * add - sums two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return a + b;
+}
+
+/**
+ * sub - subtraction between two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return a - b;
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return a * b;
+}
+
+/**
+ * div - divide two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return a / b;
+}
+
+/**
+ * add - finds the remainder of division between two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return a % b;
+}
